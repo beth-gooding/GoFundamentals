eb8deb074008eb27d3cb3511ffb80bff1d352d27
beth-gooding <beth.gooding@hotmail.com>
2024-08-09 17:47:33 +0100

    adds implementations for all of Chapter 6

---
+++ GoLearningBook/Chapter6/6.02/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	km := 2
+	if km > 2 {
+		fmt.Println("Take the car")
+	} else {
+		fmt.Println("We're going to walk today")
+	}
+}
---
+++ GoLearningBook/Chapter6/a6.04/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrInvalidLastName      = errors.New("invalid last name")
+	ErrInvalidRoutingNumber = errors.New("invalid routing number")
+)
+
+type directDeposit struct {
+	lastName      string
+	firstName     string
+	bankName      string
+	routingNumber int
+	accountNumber int
+}
+
+func (d *directDeposit) validateRoutingNumber() error {
+	if d.routingNumber < 100 {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("Printing error returned from recover:", r)
+			}
+		}()
+		panic(ErrInvalidRoutingNumber)
+	}
+	return nil
+}
+
+func (d *directDeposit) validateLastName() error {
+	// Not specific enough, should think about whitespace strings too
+	if d.lastName == "" {
+		return ErrInvalidLastName
+	}
+	return nil
+}
+
+func (d *directDeposit) report() {
+	// Can improve using strings.repeat
+	fmt.Println("*********************************************************************************************")
+	fmt.Println("Last Name:", d.lastName)
+	fmt.Println("First Name:", d.firstName)
+	fmt.Println("Bank Name:", d.bankName)
+	fmt.Println("Routing Number:", d.routingNumber)
+	fmt.Println("Account Number:", d.accountNumber)
+}
+
+func main() {
+	myDirectDeposit := directDeposit{lastName: "", firstName: "Abe", bankName: "XYZ Inc", routingNumber: 17, accountNumber: 1809}
+
+	err := myDirectDeposit.validateLastName()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = myDirectDeposit.validateRoutingNumber()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	myDirectDeposit.report()
+}
